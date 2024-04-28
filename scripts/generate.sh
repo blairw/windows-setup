b0701a4cec543c1a488b4ae309c348eb6b8caf8b
Blair Wang <entree-medias-0o@icloud.com>
2024-04-28 16:31:02 +0100

    [chore] Setup under docs/

--- scripts/generate.sh
+++
@@ -1,8 +0,0 @@
-cat docs/*.md > temp/temp1.md
-pandoc temp/temp1.md --toc --toc-depth 2 --wrap=none -s -o temp/temp2.md
-cat config/header.md > README.md
-cat temp/temp2.md >> README.md
-
-# Clean up
-# rm temp/temp1.md
-# rm temp/temp2.md
